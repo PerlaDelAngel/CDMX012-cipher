c9d1811874f3cd975943515d5549abf6d0d36b7c
Perla Del Ángel <perdelangel@gmail.com>
2022-01-26 16:56:00 -0600

    Cambios relacionados con la optimización del código

--- src/index.js
+++ src/index.js
@@ -6,35 +6,35 @@ console.log(cipher);
 // CIFRAR
 function cifrar(){
   let offset = parseInt(document.getElementById("offset").value);
-  let string = document.getElementById("mensaje").value;
+  let string = document.getElementById("message").value;
   
   document.getElementById("output").value = cipher.encode(offset, string);
 }
 
-let botonCifrar = document.getElementById("botonCifrar");
-botonCifrar.addEventListener("click", cifrar); 
+let cipherButton = document.getElementById("cipherButton");
+cipherButton.addEventListener("click", cifrar); 
 
 //DESCIFRAR
 function descifrar(){
   let offset = parseInt(document.getElementById("offset").value);
-  let string = document.getElementById("mensaje").value;
+  let string = document.getElementById("message").value;
 
   document.getElementById("output").value = cipher.decode(offset, string);
 }
 
-let botonDescifrar = document.getElementById("botonDescifrar");
-botonDescifrar.addEventListener("click", descifrar); 
+let decipherButton = document.getElementById("decipherButton");
+decipherButton.addEventListener("click", descifrar); 
 
 //COPIAR 
 function copyToClipboard(){
-  let mensajeCopiar = document.getElementById("output");
-  mensajeCopiar.select();
+  let copyMessage = document.getElementById("output");
+  copyMessage.select();
   
-  navigator.clipboard.writeText(mensajeCopiar.value);
+  navigator.clipboard.writeText(copyMessage.value);
 }
 
-let botonCopiar = document.getElementById("botonCopiar");
-botonCopiar.addEventListener("click", copyToClipboard);
+let copyButton = document.getElementById("copyButton");
+copyButton.addEventListener("click", copyToClipboard);
 
 //LIMPIAR
 function reset(){
@@ -42,12 +42,12 @@ function reset(){
   document.getElementById("formOutput").reset();
 }
 
-let botonReiniciar = document.getElementById("reiniciar");
-botonReiniciar.addEventListener("click", reset);
+let resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", reset);
 
 //POP UP 1
-let botonPop1 = document.getElementById("cifradoCesar");
-let popWrapper1 = document.getElementById("seccionCifradoCesar");
+let botonPop1 = document.getElementById("caesarCipherButton");
+let popWrapper1 = document.getElementById("caesarCipherSection");
 const cerrar1 = document.getElementById("close1");
 
 //MOSTRAR POP UP 1
@@ -61,8 +61,8 @@ cerrar1.addEventListener("click", function(){
 });
 
 //POP UP 2
-let botonPop2 = document.getElementById("uso");
-let popWrapper2 = document.getElementById("seccionSugerencias");
+let botonPop2 = document.getElementById("useSuggestions");
+let popWrapper2 = document.getElementById("suggestionsSection");
 const cerrar2 = document.getElementById("close2");
 
 //MOSTRAR POP UP 2
